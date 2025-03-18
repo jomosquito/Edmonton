eaae3a7c0b5b65bb5d71729b4819fd9fd0608800
Alexis Alejandro <58076607+AlexisA3400@users.noreply.github.com>
2025-03-18 11:48:07 -0500

    Update init.py

--- app/routes/init.py
+++ app/routes/init.py
@@ -1,1 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    
+    db.init_app(app)
+    
+    from .routes.auth_routes import auth_routes
+    from .routes.admin_routes import admin_routes
+    from .routes.user_routes import user_routes
+    from .routes.oauth_routes import oauth_routes
+    from .routes.withdrawal_routes import withdrawal_routes
+    
+    app.register_blueprint(auth_routes)
+    app.register_blueprint(admin_routes)
+    app.register_blueprint(user_routes)
+    app.register_blueprint(oauth_routes)
+    app.register_blueprint(withdrawal_routes)
+    
+    return app
